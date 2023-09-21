d0c8cfe32e4de9f8288dd8646281171601f50165
Jim Baker <jim.baker@python.org>
2023-09-20 18:16:44 -0600

    Add sanitization tests with respect to injection attacks

--- tests/test_htmltag.py
+++ tests/test_htmltag.py
@@ -1,4 +1,4 @@
-from fdom.htmltag import html, html_iter
+from fdom.htmltag import html, html_iter, HTML
 
 
 def test_simple():
@@ -60,4 +60,27 @@ def test_listing_iter():
             <ul><li>High: Get milk</li><li>High: Change tires</li></ul>
         </body>
     </html>"""
+
+
+def test_sanitize():
+    # NOTE this string is not encoded with the html tag, which is used for
+    # source code, instead this is supposed to be representative of user
+    # input that is used maliciously for an injection attempt (XSS).
+    # Example from https://en.wikipedia.org/wiki/Cross-site_scripting
+    injection_attempt = '<script>alert("xss");</script>'
+
+    # However it is properly sanitized
+    assert html'<div>Here is your query: {injection_attempt}</div>' == \
+        '<div>Here is your query: &lt;script&gt;alert(&quot;xss&quot;);&lt;/script&gt;</div>'
+
+    # FIXME currently failing to parse, then compile properly
+    assert html'<div attr="{injection_attempt}">Hello, world!</div>' is None
+
+
+def test_work_with_marker():
+    # Using HTML assures that the text is valid HTML as-is. Use carefully!
+    arbitrary_html = HTML('<script>alert("No worries");</script>')
+    assert html'<div>Here is an embedded script: {arbitrary_html}</div>' == \
+        '<div>Here is an embedded script: <script>alert("No worries");</script></div>'
+
 
